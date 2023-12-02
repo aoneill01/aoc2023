fbbcfb339b850ab24a9d7d73f2463341c1bf64dd
Andy O'Neill <aoneill01@gmail.com>
2023-12-02 08:56:59 -0500

    Day 02

--- day02/main.go
+++ day02/main.go
@@ -3,19 +3,105 @@ package main
 import (
 	"aoc2023/util"
 	"fmt"
+	"strconv"
+	"strings"
 )
+
+type cubes struct {
+	red   int
+	green int
+	blue  int
+}
+
+type game struct {
+	id       int
+	revealed []cubes
+}
 
 func main() {
 	input := util.ReadDay(2)
 
 	fmt.Println(part1(input))
-	// fmt.Println(part2(input))
+	fmt.Println(part2(input))
+}
+
+func parseLine(line string) game {
+	parts := strings.Split(line, ": ")
+	idStr, _ := strings.CutPrefix(parts[0], "Game ")
+	id, _ := strconv.Atoi(idStr)
+	game := game{id, []cubes{}}
+
+	parts = strings.Split(parts[1], "; ")
+	for _, part := range parts {
+		c := cubes{}
+		draws := strings.Split(part, ", ")
+		for _, draw := range draws {
+			a := strings.Split(draw, " ")
+			count, _ := strconv.Atoi(a[0])
+			switch a[1] {
+			case "red":
+				c.red = count
+			case "green":
+				c.green = count
+			case "blue":
+				c.blue = count
+			}
+		}
+		game.revealed = append(game.revealed, c)
+	}
+
+	return game
+}
+
+func isValidPart1(g game) bool {
+	for _, c := range g.revealed {
+		if c.red > 12 || c.green > 13 || c.blue > 14 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func fewestPossible(g game) cubes {
+	result := cubes{}
+
+	for _, c := range g.revealed {
+		if c.red > result.red {
+			result.red = c.red
+		}
+		if c.green > result.green {
+			result.green = c.green
+		}
+		if c.blue > result.blue {
+			result.blue = c.blue
+		}
+	}
+
+	return result
 }
 
 func part1(input []string) int {
-	return 0
+	var result int
+
+	for _, line := range input {
+		g := parseLine(line)
+		if isValidPart1(g) {
+			result += g.id
+		}
+	}
+
+	return result
 }
 
 func part2(input []string) int {
-	return 0
+	var result int
+
+	for _, line := range input {
+		g := parseLine(line)
+		fp := fewestPossible(g)
+		result += fp.red * fp.green * fp.blue
+	}
+
+	return result
 }
--- day02/main_test.go
+++ day02/main_test.go
@@ -1,10 +1,28 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
+
+const sample1 = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
 
 func TestPart1(t *testing.T) {
-	got := part1([]string{""})
-	want := 1
+	got := part1(strings.Split(sample1, "\n"))
+	want := 8
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	got := part2(strings.Split(sample1, "\n"))
+	want := 2286
 
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
